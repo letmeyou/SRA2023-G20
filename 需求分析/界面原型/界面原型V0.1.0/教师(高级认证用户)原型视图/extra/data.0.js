c5b1757b2e9c47d7c6ab8c12ac842992419556c3
letmeyou <irqgr23@163.com>
2023-04-21 14:05:36 +0800

    latest

---
+++ 需求分析/界面原型/界面原型V0.1.0/教师(高级认证用户)原型视图/extra/data.0.js
@@ -0,0 +1,1 @@
+window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"Ik5jEohlrt906fwgHkC8m8","projectEncryptedPassword":"","isPasswordRequired":false,"is_on_premises":false,"is_wonder_share":false};
---
+++ 需求分析/界面原型/界面原型V0.1.0/管理员原型视图/extra/data.0.js
@@ -0,0 +1,1 @@
+window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"VG3Jln2grtfz8wkCUx6Sfe","projectEncryptedPassword":"","isPasswordRequired":false,"is_on_premises":false,"is_wonder_share":false};
