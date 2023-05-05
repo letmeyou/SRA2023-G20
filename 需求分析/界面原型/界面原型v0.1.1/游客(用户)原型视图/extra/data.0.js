62adc14b00ed4bd22dead5514bbb62aa02032e38
letmeyou <irqgr23@163.com>
2023-05-05 11:26:10 +0800

    latest

---
+++ 需求分析/界面原型/界面原型v0.1.1/游客(用户)原型视图/extra/data.0.js
@@ -0,0 +1,1 @@
+window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"JSxSEIz3rt8zkf6PYIM1v3","projectEncryptedPassword":"","isPasswordRequired":false,"is_on_premises":false,"is_wonder_share":false};
